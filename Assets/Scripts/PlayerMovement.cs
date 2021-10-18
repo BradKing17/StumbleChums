27f679f619ec6de27b8e59c545d5aeb3a7e534eb
Bradley King <king.brad1997@gmail.com>
2021-10-18 13:10:31 +0100

    Basic animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,10 +12,20 @@ public class PlayerMovement : MonoBehaviour
     public float airSpeed = 5.0F;
     public float jumpStrength = 1.0F;
 
+    public bool isTopDown = false;
+
+    public Sprite[] sprites;
+    private SpriteRenderer spr;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         cc = GetComponent<CapsuleCollider>();
+        spr = GetComponentInChildren<SpriteRenderer>();
+        if (isTopDown)
+        {
+            spr.transform.Rotate(new Vector3(90, 0, 0));
+        }
     }
 
     void Update()
@@ -24,6 +34,27 @@ public class PlayerMovement : MonoBehaviour
         // Horizontal
         float horizontal = Input.GetAxis("Horizontal") * (grounded ? moveSpeed : airSpeed);
         rb.AddForce(transform.right * horizontal, ForceMode.Force);
+
+        spr.sprite = sprites[0];
+        Vector3 vel = rb.velocity;
+        if(vel.x >= -vel.z && vel.x >= 0.5F)
+        {
+            spr.sprite = sprites[2];
+        }
+        else if (vel.x <=vel.z && vel.x <= -0.5)
+        {
+            spr.sprite = sprites[1];
+        }
+        if (vel.z >= vel.x && vel.z >= 0.5F)
+        {
+            spr.sprite = sprites[3];
+        }
+        
+        if(isTopDown)
+        {
+            float vertical = Input.GetAxis("Vertical") * (grounded ? moveSpeed : airSpeed);
+            rb.AddForce(transform.forward * vertical, ForceMode.Force);
+        }
         // Jumping
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
