3564cc20159c020c1f2980f5c42f97e379cda12c
Oli <hewisonoliver@gmail.com>
2021-10-18 12:34:13 +0100

    Physics-based player movement and Rider GitIgnore

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float Speed = 10.0F;
+    private Rigidbody rb;
+    private CapsuleCollider cc;
+    
+    public float moveSpeed = 10.0F;
+    public float airSpeed = 5.0F;
+    public float jumpStrength = 1.0F;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        cc = GetComponent<CapsuleCollider>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Input.GetAxis("Horizontal") * Speed * Time.deltaTime, Input.GetAxis("Vertical") * Speed * Time.deltaTime, 0);
+        bool grounded = IsGrounded();
+        // Horizontal
+        float horizontal = Input.GetAxis("Horizontal") * (grounded ? moveSpeed : airSpeed);
+        rb.AddForce(transform.right * horizontal, ForceMode.Force);
+        // Jumping
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.AddForce(Vector2.up * jumpStrength, ForceMode.Impulse);
+        }
     }
+
+    private bool IsGrounded()
+    {
+        return rb.velocity.y <= 0 && Physics.Raycast(transform.position, Vector3.down, cc.height / 2.0F + 0.05F);
+    }
+
 }
