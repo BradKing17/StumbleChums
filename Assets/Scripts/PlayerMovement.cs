13f0dfe5a52d63cc124bf27d035cd6ec2ebcdec6
Matthew B <52146102+Xenntric@users.noreply.github.com>
2021-11-01 22:08:54 +0000

    Ragdoll Update
    CHANGES:
    - Objects the chums walk on now need to be on the 'Ground' layer (layer index 6)
    - Game Manager object needs the tag 'Game Manager'
    - Objects that cause chums to ragdoll need the tag 'ragdoll'
    
    Updates:
    - Chums will ragdoll in jumpclub, they will rotate which is definitely too
      fancy for the Atari 260. May turn that off.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,7 +13,8 @@ public class PlayerMovement : MonoBehaviour
     public float airSpeed = 5.0F;
     public float jumpStrength = 1.0F;
 
-    public bool isTopDown = false;
+    public bool GMisTopDown;
+    //public bool isTopDown = false;
     
     public Rigidbody rb;
     public CapsuleCollider cc;
@@ -21,24 +22,60 @@ public class PlayerMovement : MonoBehaviour
     public SphereCollider grabRange;
     public List<Collider> grabbables;
     public SpringJoint joint;
+    public CapsuleCollider triggerCapsuleCollider;
 
     private Vector2 moveInput;
     private float grabInput;
-    private bool grounded;
+    public bool grounded;
     private bool canDive = true;
     private bool canGrab = true;
+    public bool ragdoll = false;
+    public SpriteRenderer spriteRenderer;
+    private bool ragdollFlashing;
+
+    private RigidbodyConstraints standardContraint;
+    private RigidbodyConstraints unfreezeZConstraint;
+    public LayerMask groundLayer;
     
+    
+    private void OnEnable()
+    {
+        // This will check if the GAME MANAGER object says whether the scene is Top Down
+        GMisTopDown = GameObject.FindWithTag("Game Manager").GetComponent<GameManager>().topDown;
+        spriteRenderer = gameObject.GetComponentInChildren<SpriteRenderer>();
+        
+        // So if it is top down the PosZ constraint is unfrozen
+        if (GMisTopDown)
+        {
+            Debug.Log("Ding");
+            
+            standardContraint =  RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | 
+                                 RigidbodyConstraints.FreezeRotationY;
+        }
+        else
+        {
+            Debug.Log("Dong");
+
+            standardContraint = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX |
+                                RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
+        }
+        
+        unfreezeZConstraint = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezePositionZ |
+                              RigidbodyConstraints.FreezeRotationY;
+        rb.constraints = standardContraint;
+        
+    }
 
     void Update()
     {
         grounded = IsGrounded();
         // Horizontal
         rb.AddForce(transform.right * (moveInput.x * (grounded ? moveSpeed : airSpeed)), ForceMode.Acceleration);
-        if (isTopDown)
+        if (GMisTopDown)
         {
             rb.AddForce(transform.forward * (moveInput.y * (grounded ? moveSpeed : airSpeed)), ForceMode.Acceleration);
         }
-        Debug.Log(grabInput);
+        //Debug.Log(grabInput);
         if(grabInput > 0)
         {
             joint.connectedBody = grabbables[0].gameObject.GetComponent<Rigidbody>();
@@ -63,11 +100,17 @@ public class PlayerMovement : MonoBehaviour
         {
             spr.sprite = playerType.sprites.back;
         }
+
+        if (ragdoll && grounded)
+        {
+            StartCoroutine(RagdollCatch());
+        }
     }
 
     private bool IsGrounded()
     {
-        return Physics.Raycast(transform.position, Vector3.down, cc.height / 2.0F + 0.05F);
+        Debug.DrawRay(transform.position, Vector3.down * (cc.height / 2.0F + 0.05F), Color.green);
+        return Physics.Raycast(transform.position, Vector3.down, cc.height / 2.0F + 0.05F, groundLayer);
     }
 
     public void SetPlayerType(PlayerType _type)
@@ -78,8 +121,8 @@ public class PlayerMovement : MonoBehaviour
 
     public void SetTopDown(bool _topDown)
     {
-        isTopDown = _topDown;
-        rb.constraints = isTopDown ? 
+        GMisTopDown = _topDown;
+        rb.constraints = GMisTopDown ? 
             RigidbodyConstraints.FreezeRotation:
             RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ ;
         if (_topDown)
@@ -95,7 +138,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext _value)
     {
-        if (!isTopDown && _value.phase == InputActionPhase.Started && IsGrounded())
+        if (!GMisTopDown && _value.phase == InputActionPhase.Started && IsGrounded())
         {
             canDive = true;
             rb.AddForce(Vector2.up * jumpStrength, ForceMode.VelocityChange);
@@ -104,11 +147,11 @@ public class PlayerMovement : MonoBehaviour
 
     public void Grab(InputAction.CallbackContext _value)
     {
-       if(!grounded && canDive)
-        {
+       if(!grounded && canDive) 
+       {
             canDive = false;
             rb.AddForce(Vector2.right * rb.velocity.x * (jumpStrength/2), ForceMode.VelocityChange);
-        }
+       }
        else
        {
             grabInput = _value.ReadValue<float>();
@@ -136,5 +179,47 @@ public class PlayerMovement : MonoBehaviour
                 grabbables.Remove(other);
             }
         };
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //print("ding");
+        if (other.gameObject.CompareTag("ragdoll"))
+        {
+            StartCoroutine(StartRagdoll());
+        }
+    }
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("ragdoll") && grounded)
+        {
+            StartCoroutine(EndRagdoll());
+        }
+    }
+
+    IEnumerator EndRagdoll()
+    {
+        yield return new WaitForSeconds(.6F);
+        rb.AddForce(transform.up, ForceMode.Force);
+        gameObject.transform.rotation = new Quaternion(0, 0, 0,0);
+        rb.constraints = standardContraint;
+        spriteRenderer.color = Color.white;
+        ragdoll = false;
+    }
+
+    IEnumerator StartRagdoll()
+    {
+        ragdoll = true;
+        spriteRenderer.color = Color.red;
+        rb.constraints = unfreezeZConstraint;
+        yield return new WaitForSeconds(.1F);
+    }
+
+    // This should(?) catch whether the chum is on the ground and ragdolling, but not
+    // getting back up
+    IEnumerator RagdollCatch()
+    {
+        yield return new WaitForSeconds(.3F);
+        StartCoroutine(RagdollCatch());
     }
 }
