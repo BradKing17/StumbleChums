58029c7030ea0c542d0fc22bf16abd21d2ba8ac7
b3-kennedy <benjamin3.kennedy@live.uwe.ac.uk>
2021-11-02 15:12:26 +0000

    main menu changes

---
+++ Assets/Scripts/MenuScripts/ButtonScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    public GameObject background;
+    // Start is called before the first frame update
+    //public void OnSelect(BaseEventData eventData)
+    //{
+    //    GetComponent<Image>().color = Color.white;
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(EventSystem.current.currentSelectedGameObject == gameObject)
+        {
+            GetComponent<Image>().color = Color.white;
+        }
+        else
+        {
+            GetComponent<Image>().color = Color.black;
+        }
+    }
+}
